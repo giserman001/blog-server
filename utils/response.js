fde3e5eb3b98c3b637180b8f545ad0b38bfcaec0
liuya <1522962872@qq.com>
2020-11-04 18:38:20 +0800

    添加默认message

--- utils/response.js
+++ utils/response.js
@@ -4,7 +4,7 @@
  *
  * @example app.context.client = func , ctx.client(10, '成功')
  */
-module.exports = function(data = null, msg = '') {
+module.exports = function(data = null, msg = '请求成功') {
     const ctx = this
     ctx.response.set('Content-Type', 'application/json')
     ctx.response.body = {
